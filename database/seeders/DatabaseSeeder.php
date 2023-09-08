7c40339cbb095578d2911dd092f67881c36249fc
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-09-07 20:58:15 -0400

    seedin data done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Entry;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(MoodSeeder::class);
+        // create an array of random unique dates in the format y-m-d
+        $randomDates = [];
+        while (count($randomDates) < 15) {
+            $date = Carbon::today()->subDays(rand(0, 31))->format('Y-m-d');
+            if (!in_array($date, $randomDates))
+                array_push($randomDates, $date);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        foreach ($randomDates as $date) {
+            Entry::factory()->create([
+                'date' => $date
+            ]);
+        }
     }
 }
